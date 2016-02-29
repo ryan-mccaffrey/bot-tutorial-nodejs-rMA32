33be697cac0ed7342ba6e0f90325131e17bebb1d
Ryan McCaffrey <ryan.mccaffrey.42@gmail.com>
2016-02-29 15:45:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ function respond() {
       botRegexSiege = /^\/siege/;
       botRegexZap = /^\/zapquake/i;
       botRegexBC = /^\/bestclasher/;
-      botRegexDef = /^\/hitpoints/;
+      botRegexDef = /^\/defenseweights/;
       botRegexTS = /^\/tsinfo/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
