445b8748f01f1304d4aed8a5d91a11bd1e5fa827
Ryan McCaffrey <ryan.mccaffrey.42@gmail.com>
2016-02-29 14:44:35 -0500

    Added custom command

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ function respond() {
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/;
+      botRegexZap = /^\/zapquake/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -19,6 +20,11 @@ function respond() {
     postMessage(cool());
     this.res.end();
   } 
+  else if(request.text && botRegexZap.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/HUPYJ4N.jpg");
+    this.res.end();
+  }
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://daddyleagues.com/SRMFL/team/"+request.text.substring(5,8)+"/depthchart");
@@ -104,7 +110,7 @@ function respond() {
   }
   
   else {
-    console.log("don't care");
+    console.log("Fuck off");
     this.res.writeHead(200);
     this.res.end();
   }
