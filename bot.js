bb610f7cda07012cfc80a516d496be96d6d14819
Ryan McCaffrey <ryan.mccaffrey.42@gmail.com>
2016-02-29 15:40:34 -0500

    Added ts info and defense hitpoints commands

--- bot.js
+++ bot.js
@@ -11,6 +11,8 @@ function respond() {
       botRegexSiege = /^\/siege/;
       botRegexZap = /^\/zapquake/i;
       botRegexBC = /^\/bestclasher/;
+      botRegexDef = /^\/hitpoints/;
+      botRegexTS = /^\/tsinfo/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var defenseAb = ["AD", "IT", "ad", "it"]
@@ -36,6 +38,16 @@ function respond() {
   else if(request.text && botRegexBC.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://i.imgur.com/aFweIc0.jpg");
+    this.res.end();
+  }
+  else if(request.text && botRegexTS.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Teamspeak server info:\nAddress: ts89.gameservers.com:9163\nPassword: North");
+    this.res.end();
+  }
+  else if(request.text && botRegexDef.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/bjBdKbO.png");
     this.res.end();
   }
   else if(request.text && botRegexDL.test(request.text)) {
