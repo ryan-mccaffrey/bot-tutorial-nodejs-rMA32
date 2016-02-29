76708c22aa58fa29a3b163ecc39152257668893e
Ryan McCaffrey <ryan.mccaffrey.42@gmail.com>
2016-02-29 15:05:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,7 @@ function respond() {
       botRegexSiege = /^\/siege/;
       botRegexZap = /^\/zapquake/;
       botRegexBC = /^\/bestclasher/;
+      botRegexZqIT = /^\/zapquakeIT/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -29,6 +30,11 @@ function respond() {
   else if(request.text && botRegexBC.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://i.imgur.com/aFweIc0.jpg");
+    this.res.end();
+  }
+  else if(request.text && botRegexZqIT.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/KP2VlH1.jpg");
     this.res.end();
   }
   else if(request.text && botRegexDL.test(request.text)) {
