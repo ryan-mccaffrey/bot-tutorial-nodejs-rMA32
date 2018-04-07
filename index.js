0f044dfacaba4056cb2172e3726af565138c4510
Ryan McCaffrey <ryan.mccaffrey.42@gmail.com>
2018-04-07 03:21:47 -0400

    trying to get app to build

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ server = http.createServer(function (req, res) {
   });
 });
 
-port = Number(process.env.PORT || 3002);
+port = Number(process.env.PORT || 5000);
 server.listen(port);
 
 function ping() {
