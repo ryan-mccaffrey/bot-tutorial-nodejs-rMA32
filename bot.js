83a3cf55aba0a6d2f78cf85d4e53dd13c7c37e88
Ryan McCaffrey <ryan.mccaffrey.42@gmail.com>
2018-04-08 12:02:30 -0400

    cleaning commands

--- bot.js
+++ bot.js
@@ -16,8 +16,8 @@ var quotes       = require('./custom_commands/quotes.js');
 var atEveryone   = require('./custom_commands/at-everyone.js');
 var funCommands  = require('./custom_commands/fun-commands.js');
 var gif          = require('./custom_commands/giphy-api.js');
-var catFact      = require('./custom_commands/cat-fact.js');
-var urbanDict = require('./custom_commands/urban-dictionary.js');
+// var catFact      = require('./custom_commands/cat-fact.js');
+// var urbanDict = require('./custom_commands/urban-dictionary.js');
 
 //load config
 var config       = require('./config/config.js');
@@ -25,7 +25,7 @@ var HTTPS = require('https');
 
 
 //Temporarily just an array of the commands functions. Make an object with configuration values.
-var checkCommandsHSH = [mods, sysTriggers, userCmds, userMentions, sysCommands, atEveryone, funCommands, quotes, rooms, gif, catFact, urbanDict];
+var checkCommandsHSH = [mods, sysTriggers, userCmds, userMentions, sysCommands, atEveryone, funCommands, quotes, rooms, gif];
 
 exports.init = function() {
   var req = this.req;
